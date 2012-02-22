69ed35f438479395db1271dabbc24667facba259
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2012-02-22 18:56:31 +1000

    More progress on MVP

--- jcombo/plugins/js/mvp.js
+++ jcombo/plugins/js/mvp.js
@@ -1,10 +1,25 @@
 $j.mvp = {
 	_idCount: 0,
 	_mainView: null,
+	init: function() {
+		$j.mvp._mainView = new $j.mvp.View('{{root}}');
+		$(document.body).html($j.mvp._mainView.toString());
+	},
 	
 	setMainView: function(view) {
-		$j.mvp._mainView = view;
-		$(document.body).html(view.toString());
+		if(!$j.mvp._mainView) {
+			throw 'DOMNotReadyException: the DOM is not ready - Main view cannot be set';
+		}
+	
+		$j.mvp._mainView.setContent('root', view);
+		/*
+		if(view instanceof $j.mvp.View) {
+			$(document.body).html(view.toString());
+			view.triggerRefresh();
+		} else {
+			$(document.body).html(view);
+		}
+		*/
 	},
 	
 	generateID: function() {
@@ -13,6 +28,12 @@ $j.mvp = {
 	
 	View: function(template) {
 		var self = this;
+		self._parent = null;
+		self._callbacks = {};
+		self._unbindSelectorMap = {};
+		self._rebindSelectorMap = {};
+		self._callbacks['refresh'] = [];
+		
 		self._id = 'jComboView' + $j.mvp.generateID();
 		
 		if(typeof template == 'string') {
@@ -26,18 +47,43 @@ $j.mvp = {
 			return self._id;
 		}
 		
+		self.setParent = function(parent) {
+			self._parent = parent;
+		}
+		
+		self.getParent = function() {
+			return self._parent;
+		}
+		
 		self.setData = function(data) {
-			if(data) {
-				self._data = data;
-			} else {
-				self._data = {};
-			}
+			$.each(self._data, function(index, value) {
+				if(value instanceof $j.mvp.View) {
+					value.triggerBeforeRemove();
+				}
+			});
+			
+			$.each(data, function(index, value) {
+				if(self._data.hasOwnProperty(index) && self._data[index] instanceof $j.mvp.View) {
+					self._data[index].setParent(null);
+				}
+				if(value instanceof $j.mvp.View) {
+					value.setParent(self);
+				}
+				self._data[index] = value;
+			});
 			self._update();
 		}
 		
-		self.clearData = function() {
-			self._data = {};
-			self._update();
+		self.clearData = function(areaName) {
+			var newData = {};
+			if(areaName) {
+				newData[areaName] = '';
+			} else {
+				$.each(self._data, function(index) {
+					newData[index] = '';
+				});
+			}
+			self.setData(newData);
 		}
 		
 		self.getData = function() {
@@ -45,12 +91,19 @@ $j.mvp = {
 		}
 		
 		self.setContent = function(areaName, view) {
-			self._data[areaName] = view;
-			self._update();
+			var data = {};
+			data[areaName] = view;
+			self.setData(data);
 		}
 		
 		self.toString = function() {
 			return self._wrapID(self._getContent());
+		}
+		
+		self.select = function(selector) {
+			var elSelector = "#" + self._id + " " + selector;
+			self._unbindSelectorMap[elSelector] = true;
+			return $(elSelector);
 		}
 		
 		self.delegate = function(selector, eventType, handler) {
@@ -71,6 +124,56 @@ $j.mvp = {
 			} else {
 				$(document.body).undelegate("#" + self._id + " " + selector, eventType, handler);
 			}
+		}
+		
+		self.bind = function(eventType, handler) {
+			var selector = '#' + self._id;
+			self._rebindSelectorMap[selector] = {eventType: eventType, handler: handler};
+			$(selector).bind(eventType, handler);
+		}
+		
+		self.unbind = function(eventType, handler) {
+			if(eventType == 'refresh') {
+				self._callbacks['refresh'] = $.grep(self._callbacks['refresh'], function(value) {
+					return value != handler;
+				});
+			} else {
+				var selector = '#' + self._id;
+				self._rebindSelectorMap[selector] = null;
+				$(selector).unbind(eventType, handler);
+			}
+		}
+		
+		self.refresh = function(callback) {
+			self._callbacks['refresh'].push(callback);
+		}
+		
+		self.triggerBeforeRemove = function() {			
+			$.each(self._unbindSelectorMap, function(index) {
+				$(index).unbind();
+			});
+			
+			$.each(self._rebindSelectorMap, function(index) {
+				$(index).unbind();
+			});
+			
+			self._unbindSelectorMap = {};
+		}
+		
+		self.triggerRefresh = function() {
+			$.each(self._rebindSelectorMap, function(index, value) {
+				$(index).bind(value.eventType, value.handler);
+			});
+			
+			$.each(self._data, function(index, value) {
+				if(value instanceof $j.mvp.View) {
+					value.triggerRefresh();
+				}
+			});
+			
+			$.each(self._callbacks['refresh'], function(index, value) {
+				value();
+			});
 		}
 		
 		self._getContent = function(areaName) {
@@ -102,13 +205,15 @@ $j.mvp = {
 		}
 		
 		self._update = function() {
-			var div = $('#' + self._id);
-			if(div) {
-				div.html(self._getContent());
-			}
+			$('#' + self._id).html(self._getContent());
+			self.triggerRefresh();
 		}
 	}
 };
+
+$(document).ready(function() {
+	$j.mvp.init();
+});
 
 $j.grab.view = function(templateName, jRequest) {
 	if(jRequest) {
