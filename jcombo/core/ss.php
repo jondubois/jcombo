dbdb6ad42020b4c86668045b72be829a94327f27
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2012-04-30 20:28:32 +1000

    Added a file for interacting with server interfaces from the server side

---
+++ jcombo/core/ss.php
@@ -0,0 +1,22 @@
+<?php
+/*
+* This file serves as a server service interface to facilitate accessing server interface classes from other server side scripts.
+*/
+
+require_once(dirname(__FILE__).'/../config.php');
+
+$relDir = './';
+while(!file_exists($relDir.'config.php') || !file_exists($relDir.'prepare.php')) {
+	if(!file_exists($relDir)) {
+		throw new Exception("The server service can only be used within a valid application subdirectory");
+	}
+	$relDir .= '../';
+}
+
+define('JC_APP_DIR', $relDir);
+
+require_once(JC_APP_DIR.'config.php');
+require_once(dirname(__FILE__).'/ServerInterface.php');
+require_once(JC_APP_DIR.'prepare.php');
+
+?>
