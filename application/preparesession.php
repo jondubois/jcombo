8a2385016449167e1da063a363e400b25f92d077
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2012-06-23 20:47:18 +1000

    Update

--- application/preparesession.php
+++ application/preparesession.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('session.save_path', dirname(__FILE__).'/../jcombo/coredata/sessions/');
+ini_set('session.save_path', dirname(__FILE__).'/../jcombo/coredata/sessions');
 ini_set('session.gc_probability', 1);
 session_start();
 ?>
